d09d33f819a81c552a62bc8af602cce20d8dfdc2
ronnysempai <rokgi45@hotmail.com>
2017-02-20 13:41:43 -0500

    2017-02-20_13:41

--- app.js
+++ app.js
@@ -27,14 +27,26 @@ server.post('/api/messages', connector.listen());
     session.send("Hello World");
 });
 */
-
+var c=0;
 bot.dialog('/', [
     function (session) {
+        c=0;
         console.log(session.message.text);
         builder.Prompts.text(session, 'Hola, soy el agente de recepcion de denuncias Cual es tu nombre?');
         //builder.Prompts.text(session, 'Cual es tu nombre?');
     },
     function (session, results) {
+        c++;
         session.send('Hola %s!', results.response);
+        if(c>0){
+            // create the card based on selection
+            var selectedCardName = results.response.entity;
+            var card = createCard(selectedCardName, session);
+
+            // attach the card to the reply message
+            var msg = new builder.Message(session).addAttachment(card);
+            session.send(msg);       
+        }
+        
     }
 ]);
