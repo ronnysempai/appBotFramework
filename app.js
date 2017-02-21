7b891f983b37e5b905c517aed0ad7b0e04356160
ronnysempai <rokgi45@hotmail.com>
2017-02-21 14:58:07 -0500

    2017-02-21_14:58

--- app.js
+++ app.js
@@ -35,20 +35,15 @@ bot.dialog('/', [
         builder.Prompts.text(session, 'Hola, soy el agente de recepcion de denuncias Cual es tu nombre?');
         //builder.Prompts.text(session, 'Cual es tu nombre?');
     },
-    function (session, results) {
-        c++;
-        session.send('Hola %s!', results.response);
-        if(c>0){
+    function (session, results){
+            session.send('Hola %s!', results.response);
             // create the card based on selection
             var selectedCardName = results.response.entity;
             //var card = createCard(selectedCardName, session);
             var card = createMyCard(session);
-
             // attach the card to the reply message
             var msg = new builder.Message(session).addAttachment(card);
             session.send(msg);       
-        }
-        
     }
 ]);
 
@@ -56,9 +51,10 @@ function createMyCard(session) {
     return new builder.HeroCard(session)
         .title('Recepcion de Denuncias')
         .subtitle(' — ')
-        .text('Recepcion de denuncias.')
+        .text('-')
         .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+            //builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+            builder.CardImage.create(session, 'https://lh6.ggpht.com/U0n-NfYLqO7WMRHElgPgKyXDtDbwwzzAznk2HrL5o-rXzy-N-uqQ0qWVKDkWWz8TAaM=w300')
         ])
         .buttons([
             builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
