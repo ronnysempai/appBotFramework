1a9e98f984d5351f4410048b0311bc34b914258a
ronnysempai <rokgi45@hotmail.com>
2017-03-03 12:42:19 -0500

    2017-03-03_12:42

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var Promise = require('bluebird');
 var request = require('request-promise').defaults({ encoding: null });
+var http = require('http');
+var fs = require('fs');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -17,7 +19,15 @@ var connector = new builder.ChatConnector({
     appId: '661ed9ab-edf1-4d04-99a8-8eb99ec982dd',//process.env.MICROSOFT_APP_ID,
     appPassword: 'HfytdoRJWjeubfuUkXzYEhV'//process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Hola.");
+        session.beginDialog('rootMenu');
+    },
+    function (session, results) {
+        session.endConversation("Fue un placer servirle.");
+    }
+]);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
@@ -29,29 +39,51 @@ server.post('/api/messages', connector.listen());
 });
 */
 var c=0;
-var cadenaMenu="Si deseas enviar una imagen ingresa 1";
-bot.dialog('/', [
+var card=1;
+var selectedCardName=3;
+var recibiendoImagen=0;
+var comisarias='Informacion de Comisarias';
+var hospitales='Informacion de Hospitales';
+var denuncias='Denuncias';
+var CardNames = [comisarias,hospitales,denuncias];
+
+bot.dialog('rootMenu', [
     function (session) {
-        c=0;
         console.log(session.message.text);
-        builder.Prompts.text(session, 'Hola, soy el agente de recepcion de denuncias Cual es tu nombre?');
-        //builder.Prompts.text(session, 'Cual es tu nombre?');
-        
+        recibirImagen(session);
+        builder.Prompts.choice(session, 'Buenos dias,este es el canal informativo de atencion ciudadana'
+        +' puedes elegir una opcion de la siguiente lista ,Que opcion quieres?', CardNames, {
+            maxRetries: 3,
+            retryPrompt: 'disculpe, se ingreso una opcion invalida'
+        });
+
     },
     function (session, results){
-            //session.send('Hola %s!', results.response);
-            //session.send('%s',cadenaMenu);
-            // create the card based on selection
-            //var selectedCardName = results.response.entity;
-            //var card = createCard(selectedCardName, session);
-            //var card = createMyCard(session);
-            // attach the card to the reply message
-            //var msg = new builder.Message(session).addAttachment(card);
-            //session.send(msg);       
-            recibirImagen(session);
+        
+        selectedCardName = results.response.entity;
+        console.log(results.response);
+        if(selectedCardName!=denuncias){
+            card = seleccionarOpcion(selectedCardName, session);
+        // attach the card to the reply message
+        var msg = new builder.Message(session).addAttachment(card);
+        session.send(msg);    
+        }else{
+            console.log('*************** imagen***********************');
+            session.send("Ahora ud puede enviar la imagen de su denuncia.");
+            session.beginDialog('recibirImagen');
+        }
     }
 ]);
 
+bot.dialog('recibirImagen', [
+    function (session) {
+        recibirImagen(session);
+    },
+    function (session, results) {
+        
+        
+    }
+]);
 function createMyCard(session) {
     return new builder.HeroCard(session)
         .title('Atencion Ciudadana')
@@ -62,18 +94,47 @@ function createMyCard(session) {
             builder.CardImage.create(session, 'https://lh6.ggpht.com/U0n-NfYLqO7WMRHElgPgKyXDtDbwwzzAznk2HrL5o-rXzy-N-uqQ0qWVKDkWWz8TAaM=w300')
         ])
         .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Enviar una foto')
+            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Mas Informacion')
         ]);
 }
 
+function createCardInformacion(session){
+    var listaComisarias="Distrito 1-Sur Lugar: Instalaciones de la Comisaría del distrito, ubicado en la ciudadela Nueve de Octubre, calle Sexta y Av. Séptima (atrás de APROFE).(Contacto: Comisario de Policía, Abg. Félix Lavayen Consuegra, 0999642149)"
+    +" <b>-</b>"
+    +"Distrito 2-Esteros Lugar: Instalaciones de la Comisaría del distrito, ubicada en la Unidad de Vigilancia Comunitaria (UVC), en la ciudadela Los Esteros diagonal al colegio José María Egas. (Contacto: Comisario de Policía, Abg. Luis Vivar Gaybort, 0997953241)";
+    
+    return new builder.HeroCard(session)
+        .title('Comisarias Comisarias')
+        .subtitle('—')
+        .text(listaComisarias)
+        .images([
+            //builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+            builder.CardImage.create(session, 'https://lh6.ggpht.com/U0n-NfYLqO7WMRHElgPgKyXDtDbwwzzAznk2HrL5o-rXzy-N-uqQ0qWVKDkWWz8TAaM=w300')
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Mas Informacion')
+        ]);
+}
+function seleccionarOpcion(selectedCardName, session) {
+    switch (selectedCardName) {
+        case comisarias:
+            return createCardInformacion(session);
+        case hospitales:
+            return null;
+        case denuncias:
+            return 1//recibirImagen(session);    
+        default:
+            return 1//recibirImagen(session);
+    }
+}
 
 /**/
 /*
 */
 function recibirImagen(session){
-        var msg = session.message;
-    if (msg.attachments.length) {
 
+    var msg = session.message;
+    if (msg.attachments.length){
         // Message with attachment, proceed to download it.
         // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
         var attachment = msg.attachments[0];
@@ -83,24 +144,30 @@ function recibirImagen(session){
 
         fileDownload.then(
             function (response) {
-
                 // Send reply with attachment type & size
                 var reply = new builder.Message(session)
                     .text('Attachment of %s type and size of %s bytes received.', attachment.contentType, response.length);
+                console.log(attachment);
+                guardarImagen(attachment.contentUrl);    
                 session.send(reply);
-
+                session.endDialog("");
             }).catch(function (err) {
                 console.log('Error downloading attachment:', { statusCode: err.statusCode, message: err.response.statusMessage });
             });
 
-    } else {
-
+    } /*else {
         // No attachments were sent
         var reply = new builder.Message(session)
-            .text('Hi there! This sample is intented to show how can I receive attachments but no attachment was sent to me. Please try again sending a new message with an attachment.');
+        .text('Ahora ud puede enviar la imagen de su denuncia.');
         session.send(reply);
-    }
-
+    }*/
+    
+}
+function guardarImagen(url){
+    var file = fs.createWriteStream("file.jpg");
+    var request = http.get(url, function(response) {
+        response.pipe(file);
+    });
 }
 // Request file with Authentication Header
 var requestWithToken = function (url) {
