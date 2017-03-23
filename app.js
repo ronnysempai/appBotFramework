ac74216e7ec4d119869ca7498377dec9fddf20fd
ronnysempai <rokgi45@hotmail.com>
2017-03-23 10:00:43 -0500

    2017-03-23T09:59

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ bot.dialog('denuncias', [
 bot.dialog('ingreseTexto', [
     function (session) {
     builder.Prompts.text(session, "Por favor envie su Ubicacion.");
-    var data = { method: "sendMessage", parameters: { text: "<b>Por favor Comparta su Ubicacion.</b>", parse_mode: "HTML", reply_markup: { keyboard: [ [ { text: "Comparta su Ubicacion", request_location: true } ],[ { text: "Comparta su Contacto", request_contact: true } ] ] } } };
+    var data = { method: "sendMessage", parameters: { text: "<b>Por favor Comparta su Ubicacion o su Contacto.</b>", parse_mode: "HTML", reply_markup: { keyboard: [ [ { text: "Comparta su Ubicacion", request_location: true } ],[ { text: "Comparta su Contacto", request_contact: true } ] ] } } };
     const message = new builder.Message(session);
     message.setChannelData(data);
     session.send(message);
