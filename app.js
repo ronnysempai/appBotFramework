07b3b9547e58925a24594547d8ba48a9bc78fabc
ronnysempai <rokgi45@hotmail.com>
2017-03-29 16:11:40 -0500

    2017-03-29T16:11

--- app.js
+++ app.js
@@ -83,7 +83,10 @@ bot.dialog('rootMenu', [
             maxRetries: 3,
             retryPrompt: 'disculpe, se ingreso una opcion invalida'
         });
-        hacerMarkup(session);
+        var data = { method: "sendMessage", parameters: { text: "<b>Por favor Comparta su Ubicacion o su Contacto.</b>", parse_mode: "HTML", reply_markup: { keyboard: [ [ { text: "Comparta su Ubicacion", request_location: true } ],[ { text: "Comparta su Contacto", request_contact: true } ] ] } } };
+        const message = new builder.Message(session);
+        message.setChannelData(data);
+        session.send(message);
     },
     function (session, results){
         
